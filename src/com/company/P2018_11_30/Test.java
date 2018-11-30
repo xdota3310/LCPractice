7ee281c0f3c0b3e548f32f8ff466b52ef0b3a63c
xsj <shijie.xu@ucarinc.com>
2018-11-30 17:56:10 +0800

    求素数的个数 两种算法比较

--- src/com/company/P2018_11_30/Test.java
+++ src/com/company/P2018_11_30/Test.java
@@ -3,7 +3,7 @@ package com.company.P2018_11_30;
 import java.util.BitSet;
 
 /**
- * 求 小于200W的素数的个数
+ * 求2~2000000之间素数的个数
  *
  * @author shijie.xu
  * @since 2018年11月30日
@@ -22,13 +22,12 @@ public class Test {
             if(j == i) {
                 //当有被整除的数字时，判断它是不是自身,若是，则说明是素数
                 // 如果是就打印出数字
-//                System.out.println(i);
                 count++;
             }
         }
         Long end = System.currentTimeMillis();
         System.out.println(count + " primes");
-        System.out.println(end - start+" ms");
+        System.out.println(end - start + " ms");
     }
 
     public static void t2() {
@@ -41,6 +40,7 @@ public class Test {
             b.set(i);
         }
         i = 2;
+//        排除所有非素数
         while(i * i <= n) {
             if(b.get(i)) {
                 count++;
@@ -52,16 +52,49 @@ public class Test {
             }
             i++;
         }
+//        在大于i小于等于n的范围中遍历剩下的素数
         while(i <= n) {
             if(b.get(i)) {
-//                System.out.println(i);
                 count++;
             }
             i++;
         }
         Long end = System.currentTimeMillis();
         System.out.println(count + " primes");
-        System.out.println(end - start+" ms");
+        System.out.println(end - start + " ms");
+    }
+
+    public static void t3() {
+        int n = 2000000;
+        Long start = System.currentTimeMillis();
+        int[] b = new int[n + 10];
+        int count = 0;
+        int i = 2;
+        for(; i <= n - 2; i++) {
+            b[i] = i;
+            i++;
+        }
+        i = 2;
+        while(i * i <= n) {
+            if(b[i] != -1) {
+                count++;
+                int k = i * 2;
+                while(k <= n) {
+                    b[k] = -1;
+                    k += i;
+                }
+            }
+            i++;
+        }
+        while(i <= n) {
+            if(b[i] != -1) {
+                count++;
+            }
+            i++;
+        }
+        Long end = System.currentTimeMillis();
+        System.out.println(count + " primes");
+        System.out.println(end - start + " ms");
     }
 
 
@@ -70,8 +103,10 @@ public class Test {
          * 测试范围：2~2000000
          * t1的运行时间：约372954 ms
          * t2的运行时间：约38 ms
+         * t3与t2运行时间近乎一致
          * */
-        t1();
+//        t1();
         t2();
+        t3();
     }
 }
