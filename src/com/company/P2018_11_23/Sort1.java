ee118dde188a8783b6ad147f213aa371824a5564
xsj <shijie.xu@ucarinc.com>
2019-06-18 18:17:05 +0800

    堆排

--- src/com/company/P2018_11_23/Sort1.java
+++ src/com/company/P2018_11_23/Sort1.java
@@ -268,6 +268,72 @@ public class Sort1 {
 
     }
 
+    private static void heapSort(int[] arr) {
+        // 将待排序的序列构建成一个大顶堆
+        for (int i = arr.length / 2; i >= 0; i--){
+            heapAdjust(arr, i, arr.length);
+        }
+
+        // 逐步将每个最大值的根节点与末尾元素交换，并且再调整二叉树，使其成为大顶堆
+        for (int i = arr.length - 1; i > 0; i--) {
+            // 将堆顶记录和当前未经排序子序列的最后一个记录交换
+            swap(arr, 0, i);
+            // 交换之后，需要重新检查堆是否符合大顶堆，不符合则要调整
+            heapAdjust(arr, 0, i);
+        }
+    }
+
+    /**
+     * 构建堆的过程
+     * @param arr 需要排序的数组
+     * @param i 需要构建堆的根节点的序号
+     * @param n 数组的长度
+     */
+    private static void heapAdjust(int[] arr, int i, int n) {
+        int child;
+        int father;
+        for (father = arr[i]; leftChild(i) < n; i = child) {
+            child = leftChild(i);
+
+            // 如果左子树小于右子树，则需要比较右子树和父节点
+            if (child != n - 1 && arr[child] < arr[child + 1]) {
+                child++; // 序号增1，指向右子树
+            }
+
+            // 如果父节点小于孩子结点，则需要交换
+            if (father < arr[child]) {
+                arr[i] = arr[child];
+            } else {
+                break; // 大顶堆结构未被破坏，不需要调整
+            }
+        }
+        arr[i] = father;
+    }
+
+    /**
+     * 获取到左孩子结点
+     *
+     * @param i
+     * @return
+     */
+    private static int leftChild(int i) {
+        return 2 * i + 1;
+    }
+
+    /**
+     * 交换元素位置
+     *
+     * @param arr
+     * @param index1
+     * @param index2
+     */
+    private static void swap(int[] arr, int index1, int index2) {
+        int tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
+    }
+
+
 
     private static void print(int[] t) {
 //        for(int tt : t) {
@@ -285,27 +351,28 @@ public class Sort1 {
     }
 
     public static void main(String[] args) {
-//        int a[] = {4, 6, 2, 53, 43, 2, 354, 34637, 1, 2, 34, 436, 567, 8, 8, 789};
-        int[] t = getArray(100000);
-        int[] a = new int[100000];
-        int[] b = new int[100000];
-        int[] c = new int[100000];
-        int[] d = new int[100000];
-        int[] e = new int[100000];
-        int[] f = new int[100000];
-        System.arraycopy(t, 0, a, 0, t.length);
-        System.arraycopy(t, 0, b, 0, t.length);
-        System.arraycopy(t, 0, c, 0, t.length);
-        System.arraycopy(t, 0, d, 0, t.length);
-        System.arraycopy(t, 0, e, 0, t.length);
-        System.arraycopy(t, 0, f, 0, t.length);
-
-        dubble(a);
-        insertionSort(b);
-        insertionSort2(c);
-        shellSort(d);
-        mergeSort(e);
-        quickSort(f);
+        int[] a = {4, 6, 2, 53, 43, 2, 354, 34637, 1, 2, 34, 436, 567, 8, 8, 789};
+//        int[] t = getArray(100000);
+//        int[] a = new int[100000];
+//        int[] b = new int[100000];
+//        int[] c = new int[100000];
+//        int[] d = new int[100000];
+//        int[] e = new int[100000];
+//        int[] f = new int[100000];
+//        System.arraycopy(t, 0, a, 0, t.length);
+//        System.arraycopy(t, 0, b, 0, t.length);
+//        System.arraycopy(t, 0, c, 0, t.length);
+//        System.arraycopy(t, 0, d, 0, t.length);
+//        System.arraycopy(t, 0, e, 0, t.length);
+//        System.arraycopy(t, 0, f, 0, t.length);
+//
+//        dubble(a);
+//        insertionSort(b);
+//        insertionSort2(c);
+//        shellSort(d);
+//        mergeSort(e);
+//        quickSort(f);
+        heapSort(a);
     }
 
 }
