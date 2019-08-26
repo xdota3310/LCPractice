59ca9e3378cd78ef1dc07c8b14dcd32491456a61
xsj <shijie.xu@ucarinc.com>
2019-08-26 18:13:26 +0800

    threadlocal

---
+++ src/main/java/com/august/threadLocal/Threadlll.java
@@ -0,0 +1,27 @@
+package com.august.threadLocal;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+/**
+ * ThreadLocal
+ *
+ * @author shijie.xu
+ * @since 2019年08月26日
+ */
+public class Threadlll {
+
+    public static void main(String[] args) {
+
+    }
+
+    static class SafeDate {
+        static final ThreadLocal<DateFormat> THREAD_LOCAL = ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+
+        static DateFormat get() {
+            return THREAD_LOCAL.get();
+        }
+
+
+    }
+}
