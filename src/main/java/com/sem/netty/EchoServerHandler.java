264781082beba7911043713d20089057223759e0
xsj <shijie.xu@ucarinc.com>
2019-09-02 17:40:13 +0800

    netty 基本流程框架

---
+++ src/main/java/com/sem/netty/EchoServerHandler.java
@@ -0,0 +1,50 @@
+package com.sem.netty;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * socket连接处理器
+ *
+ * @author shijie.xu
+ * @since 2019年09月02日
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+
+    /**
+     * 处理读事件
+     *
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ctx.write(msg);
+    }
+
+    /**
+     * 处理读完成事件
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    /**
+     * 异常处理
+     *
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        super.exceptionCaught(ctx, cause);
+    }
+}
---
+++ src/main/java/com/sem/netty/NettyEcho.java
@@ -0,0 +1,58 @@
+package com.sem.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+
+/**
+ * 基于Netty实现了echo程序服务端
+ *
+ * 首先创建了一个事件处理器（等同于Reactor模式中的事
+ * 件处理器），然后创建了bossGroup和workerGroup，再之后创建并初始化了ServerBootstrap
+ *
+ * @author shijie.xu
+ * @since 2019年09月02日
+ */
+public class NettyEcho {
+    /**
+     * 事件处理器
+     */
+    final EchoServerHandler echoServerHandler = new EchoServerHandler();
+    /**
+     * boss线程组
+     */
+    EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+    /**
+     * woker线程组
+     */
+    EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+    public void dosth() {
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+            .channel(NioServerSocketChannel.class)
+            .childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel socketChannel) throws Exception {
+                    socketChannel.pipeline().addLast(echoServerHandler);
+                }
+            });
+            ChannelFuture channelFuture = serverBootstrap.bind(9090).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+//            终⽌⼯作线程组
+            workerGroup.shutdownGracefully();
+//            终⽌boss线程组
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+}
